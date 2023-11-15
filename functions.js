11dfd10c72d56943c020de9b7b491303d130a72d
Khushi <khushijukanti33@gmail.com>
2023-11-15 22:00:45 +0530

    Full stack devlopment JS session added

---
+++ functions.js
@@ -0,0 +1,109 @@
+// function Myfunction(){
+//    let a = Number(prompt("enter the first nbr"))
+//    let b = Number(prompt("enter the second nbr"))
+//    console.log(`sum of two nbrs: ${a+b}`)
+
+// }
+// Myfunction()
+
+// ......................................................
+// function greet(msg){
+//     console.log("HIII", msg)
+// }
+// greet("How are you?")
+// greet("What are you doing?")
+
+// .........................................................
+// function dummy(param){
+//     console.log(param)
+//     return;
+// }
+
+// dummy(90)
+// dummy(`Hi`)
+// dummy([10,20,30,40])
+// dummy('gdsfgdshb')
+
+// ..........................................................
+// function ordernow(a){
+//     console.log("placing order")
+
+//     paynow()
+// }                                             // placing order
+// functions.js:30                                //paying now
+// function paynow(){
+//     console.log("paying now")
+
+// }
+// ordernow(paynow)
+
+// ................................................................
+// function greet(){
+//     console.log("Hiii")
+// }
+// function dummy(){
+//     greet()
+// }
+// let results = dummy()
+// console.log(results)
+
+
+// function data(){
+//     console.log("khushi")
+// }
+// data()
+
+// ...............................................................
+
+// function add(){
+//     // console.log(arguments)
+//     console.log(arguments[0],arguments[1])
+// }
+// add(10,20,30,40)
+// add(100,200,300,400)
+// .................................................................................
+
+// function registration(Name,age,phone,collegeName="PEC Hyderabad"){
+//     console.log(Name,age,phone,collegeName)
+// }
+// registration("Khushi",22,9052458008)
+// ...................................................................
+
+// let ages = [10,20,30,40,50,]
+// console.log(ages)
+// console.log(ages[4])
+
+// for(let i = 0;i<ages.length;i++){
+//     console.log(ages[i])
+// }
+
+// ...............................................................
+
+// let Numbers=[2,3,4,5,6,7,8,9,10]
+// function checkeven(){
+//     for(let i=0;i<=10;i++){
+//         if(Numbers[i]%2===0){
+//             console.log(Numbers[i])
+//         }
+//     }
+// }
+// checkeven()
+
+// ................................................................
+
+// let users = [{
+//     name: "Khushi",
+//     age: 22,
+//     salary: 50000
+// },
+// {
+//     name: "Ishu",
+//     age: 22,
+//     salary: 100000
+// }]
+// console.log(users[0].name)
+// console.log(users[1].name)
+
+
+
+
